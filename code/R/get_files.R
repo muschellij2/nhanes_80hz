14e7215e1f19f4f8fd6aaaff098c998ea705c9b3
muschellij2 <muschellij2@gmail.com>
2023-07-18 15:49:48 -0400

    all files to download

--- code/R/get_files.R
+++ code/R/get_files.R
@@ -19,7 +19,7 @@ df = df[sample(nrow(df)),]
 df = df %>%
   dplyr::filter(!file.exists(file))
 
-max_n = min(1000, nrow(df))
+# max_n = min(1000, nrow(df))
 # for (index in seq(nrow(df))) {
 for (index in seq(max_n)) {
   print(index)
