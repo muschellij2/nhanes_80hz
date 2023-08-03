519bf3dfdc49eccdc9f674332401a632f3176273
muschellij2 <muschellij2@gmail.com>
2023-08-02 20:52:55 -0400

    added all the XPT downloader

--- code/shell/pipeline.sh
+++ code/shell/pipeline.sh
@@ -4,6 +4,7 @@ Rnosave code/R/get_filenames.R -N FILENAMES
 Rnosave code/R/get_files.R -N GET_FILES -l mem_free=2G,h_vmem=3G -t 1-56
 
 # Converting the tarball
-Rnosave code/R/tarball_to_csv.R -N TARBALL -t 50-56 -l mem_free=20G,h_vmem=21G
+Rnosave code/R/tarball_to_csv.R -N TARBALL -t 1-56 -l mem_free=20G,h_vmem=21G
 
-Rnosave code/R/run_process.R -N PROC -t 1-56 -l mem_free=20G,h_vmem=21G
+Rnosave code/R/run_process.R -N PROC -hold_jid_ad TARBALL -t 1-56 -l mem_free=20G,h_vmem=21G
+
