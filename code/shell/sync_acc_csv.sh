499a7ac06f2aeb52fe1c88db27843963b5fe9002
muschellij2 <muschellij2@gmail.com>
2023-10-11 12:48:10 -0400

    sync all test_acc

--- code/shell/sync_acc_csv.sh
+++ code/shell/sync_acc_csv.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rsync --progress -r data/acc_csv/pax_y data/test_acc_csv
+# rsync --progress -r data/acc_csv/pax_y data/test_acc_csv
+rsync --progress -r data/acc_csv/pax_g data/test_acc_csv
+rsync --progress -r data/acc_csv/pax_h data/test_acc_csv
