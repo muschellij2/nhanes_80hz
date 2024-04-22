d0713a1a6cc1d5d1f7986e0e495bdc1bf8df21ba
muschellij2 <muschellij2@gmail.com>
2024-04-22 10:17:01 -0400

    added stops for fails

--- code/R/run_nonwear_swan.R
+++ code/R/run_nonwear_swan.R
@@ -37,34 +37,40 @@ for (i in seq_len(nrow(df))) {
 
   if (!file.exists(idf$nonwear_swan_file)) {
     data = read_80hz(file)
-    nonwear = swan::swan(df = data, sampling_rate = 80L)
-    out = nonwear$first_pass %>%
-      select(HEADER_TIMESTAMP = header_time_stamp,
-             first_pass_predicted = predicted)
-    sp = nonwear$second_pass %>%
-      select(HEADER_TIMESTAMP = header_time_stamp,
-             prediction,
-             predicted,
-             prob_sleep,
-             prob_wear,
-             prob_nonwear = prob_nwear,
-             prob_sleep_smooth,
-             prob_wear_smooth,
-             prob_nonwear_smooth = prob_nwear_smooth
-      )
-    rm(nonwear)
-    rm(data)
-    sp$prob_sleep_smooth = sapply(sp$prob_sleep_smooth, identity)
-    sp$prob_wear_smooth = sapply(sp$prob_wear_smooth, identity)
-    sp$prob_nonwear_smooth = sapply(sp$prob_nonwear_smooth, identity)
-    out = full_join(sp, out)
-    out = out %>%
-      mutate(wear = !prediction %in% "Nonwear",
-             wear = ifelse(is.na(prediction) | prediction %in% "Unknown",
-                           NA, wear)
-      ) %>%
-      select(HEADER_TIMESTAMP, wear, everything())
-    stopifnot(!any(sapply(out, is.list)))
-    write_csv_gz(out, idf$nonwear_swan_file)
+    nonwear = try({
+      swan::swan(df = data, sampling_rate = 80L)
+    })
+    if (!inherits(nonwear, "try-error")) {
+      out = nonwear$first_pass %>%
+        select(HEADER_TIMESTAMP = header_time_stamp,
+               first_pass_predicted = predicted)
+      sp = nonwear$second_pass %>%
+        select(HEADER_TIMESTAMP = header_time_stamp,
+               prediction,
+               predicted,
+               prob_sleep,
+               prob_wear,
+               prob_nonwear = prob_nwear,
+               prob_sleep_smooth,
+               prob_wear_smooth,
+               prob_nonwear_smooth = prob_nwear_smooth
+        )
+      rm(nonwear)
+      rm(data)
+      sp$prob_sleep_smooth = sapply(sp$prob_sleep_smooth, identity)
+      sp$prob_wear_smooth = sapply(sp$prob_wear_smooth, identity)
+      sp$prob_nonwear_smooth = sapply(sp$prob_nonwear_smooth, identity)
+      out = full_join(sp, out)
+      out = out %>%
+        mutate(wear = !prediction %in% "Nonwear",
+               wear = ifelse(is.na(prediction) | prediction %in% "Unknown",
+                             NA, wear)
+        ) %>%
+        select(HEADER_TIMESTAMP, wear, everything())
+      stopifnot(!any(sapply(out, is.list)))
+      write_csv_gz(out, idf$nonwear_swan_file)
+    } else {
+      rm(data)
+    }
   }
 }
