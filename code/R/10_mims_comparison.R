0c8caf29dd6cc783e99ada1d3378c3cf0fbd9368
muschellij2 <muschellij2@gmail.com>
2025-09-18 23:37:02 -0400

    added mims_comparison

---
+++ code/R/10_mims_comparison.R
@@ -0,0 +1,77 @@
+#!/usr/bin/env Rscript
+library(dplyr)
+library(tidyr)
+library(agcounter)
+library(arrow)
+library(readr)
+library(SummarizedActigraphy)
+options(digits.secs = 3)
+source(here::here("code", "R", "helper_functions.R"))
+source(here::here("code", "R", "utils.R"))
+fold = NULL
+rm(list = c("fold"))
+
+df = readRDS(here::here("data", "raw", "all_filenames.rds"))
+xdf = df
+
+allow_truncation = FALSE
+name_folder = ifelse(allow_truncation, "mims_truncation", "mims")
+df = df %>%
+  dplyr::mutate(
+    file_mims = here::here("data", name_folder, version, paste0(id, ".csv.gz"))
+  )
+
+df = df %>%
+  filter(file.exists(file_mims))
+
+xpts = tibble(
+  id = c("PAXMIN_Y", "PAXMIN_G", "PAXMIN_H"),
+  file = here::here("data", "raw_min", paste0(id, ".XPT")),
+  outfile = here::here("data", "raw_min", paste0(id, ".parquet"))
+) %>%
+  mutate(version = paste0("pax_", tolower(sub(".*_(.*)", "\\1", id))))
+
+ss = split(xpts$outfile, xpts$version)
+
+max_n = nrow(df)
+index = 1
+
+for (index in seq(max_n)) {
+  # print(index)
+  idf = df[index,]
+  print(paste0(index, " of ", max_n))
+  print(idf$csv_file)
+
+  if (
+    file.exists(idf$file_mims)
+  ) {
+
+    ds <- open_dataset(ss[[idf$version]])
+    paxmims = ds %>%
+      filter(SEQN %in% idf$id) %>%
+      collect()
+
+    mims = read_csv(idf$file_mims)
+    stopifnot(nrow(mims) == nrow(paxmims))
+
+    mims = mims %>%
+      select(time = HEADER_TIME_STAMP, MIMS_UNIT)
+    stopifnot(nrow(paxmims) == nrow(mims))
+    paxmims$MIMS_UNIT = mims$MIMS_UNIT
+
+    paxmims = paxmims %>%
+      mutate(diff = abs(MIMS_UNIT - PAXMTSM),
+             pct_diff = diff / ((MIMS_UNIT + PAXMTSM)/2) * 100) %>%
+      select(PAXDAYM, PAXSSNMP, PAXTSM, PAXMTSM, MIMS_UNIT, diff,
+             pct_diff)
+
+    head(paxmims)
+
+    stopifnot(all(paxmims$pct_diff < 1.5 | paxmims$diff < 0.1))
+
+  }
+}
+
+
+
+
